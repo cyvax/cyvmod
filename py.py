6887be963dd03ccafbad6d312bc41db4efa20257
cyvax <cyvax.123@gmail.com>
2018-05-11 15:04:20 +0200

    11/05/2018
    11/05/2018

---
+++ py.py
@@ -0,0 +1,8 @@
+from random import randint
+nlaunch = 1000
+sucess = 0
+for i in range(nlaunch)
+    if randint(1,6) + randint(1,6) == 7
+    sucess = sucess + 1
+frequence = nlaunch / sucess
+print(frequence)
